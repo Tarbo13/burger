d70dada099aa9800a1eda985ec9559eca8776585
Ben Robinson <robinson.dri@gmail.com>
2020-12-05 20:35:10 -0800

    heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var exphbs = require("express-handlebars");
 let router = require("./controllers/burgers_controller");
 
 let app = express();
-let PORT = process.env.PORT || 3000;
+let PORT = process.env.PORT || 3001;
 
 app.use(express.static("public"));
 
